8b6b54d97a6ffb7dbe6514a3b749ce809e8caf0a
Federico <federiconunezfrau@yahoo.com>
2020-08-01 22:12:40 -0300

    Notch IIR calculado a mano

--- TP_comun.m
+++ TP_comun.m
@@ -12,14 +12,18 @@ load h_sys.mat;
 fs = 44.1e3;
 nfft = 4*2048;
 
+k = 2;
+
+
+
 % Ejercicio 2
 % Se importa el filtro Notch FIR
 
 load notch_fir.mat;
 
-% Se importa el filtro Notch IIR
+% Se importan los filtros Notch IIR
 
-
+load notch_iir.mat;
 
 % Ejercicio 3
 % Se importa el equalizador FIR
@@ -32,7 +36,7 @@ load equalizador_iir.mat;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Se lee el archivo de pista
-[x,fs]=audioread('CANCIONES/pista_03.wav');
+[x,fs]=audioread('CANCIONES/pista_01.wav');
 x = x';
 
 % Se leen las interferencias
@@ -60,8 +64,8 @@ noise = A1*cos(2*pi*f1*t) + A2*cos(2*pi*f2*t) + A3*cos(2*pi*f3*t);
 
 x_2 = x_1 + noise;
 
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if(k==1)
 % Primer caso: filtro notch FIR + ecualizador FIR
 
 % Se aplica el filtro notch FIR
@@ -91,28 +95,38 @@ legend('Sistema total','Límite +2 dB','Límite -2 dB')
 xlabel('\omega / \pi (Frecuencia normalizada)');
 ylabel('Amplitud [dB]');
 title('Sistema total');
-% Se grafica el espectro de la señal en sus etapas
-
-% Señal original
-
-%y = fft(x);
-%y = y./length(x);
-%w = linspace(0,2*pi,length(x));
 %
-%figure
-%hold on;
-%plot(w/pi,y,'b-','linewidth',1);
-%grid on;
-%xlabel('\omega / \pi (Frecuencia normalizada)');
-%ylabel('Amplitud');
-%title("Espectro de la señal original");
-%xlim([0 1]);
+%% Se reproduce la señal en todas sus etapas
+%sound(x,fs);    % Señal original
+%sound(x_1,fs);  % Señal luego del sistema electroacústico
+%sound(x_2,fs);  % Señal luego del sistema electroacústico más ruido
+%sound(x_31,fs); % Señal luego del notch
+%sound(x_41,fs); % Señal luego del filtro ecualizador
+endif
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Segundo caso: filtro notch IIR + ecualizador IIR
+if(k==2)
+% Se aplica el primer filtro Notch IIR
+
+x_321 = filter(numerador_f1,denominador_f1,x_2);
+x_322 = filter(numerador_f2,denominador_f2,x_321);
+x_32 = filter(numerador_f3,denominador_f3,x_322);
+
+%Se reproduce la señal en todas sus etapas
+%sound(x,fs);    % Señal original
+%sound(x_1,fs);  % Señal luego del sistema electroacústico
+%sound(x_2,fs);  % Señal luego del sistema electroacústico más ruido
+%sound(x_32,fs); % Señal luego del notch
+
+y = fft(noise,nfft/4);
 
 
+w = linspace(0,2*pi,nfft/4);
 
-% Se reproduce la señal en todas sus etapas
-sound(x,fs);    % Señal original
-sound(x_1,fs);  % Señal luego del sistema electroacústico
-sound(x_2,fs);  % Señal luego del sistema electroacústico más ruido
-sound(x_31,fs); % Señal luego del notch
-sound(x_41,fs); % Señal luego del filtro ecualizador
+figure
+hold on;
+plot(w/pi,abs(y));
+xlim([0 1]);
+grid on;
+
+endif
